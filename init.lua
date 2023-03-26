13a2d30cfc635f2c7753c2212c9f7b791207115c
th7mo <dev@th7mo.com>
2023-03-26 09:56:09 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+require "th7mo.options"
+require "th7mo.keymaps"
+require "th7mo.plugins"
+vim.cmd "colorscheme gruvbox"
+
---
+++ lua/th7mo/options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true						-- show linenumbers in left gutter
+vim.opt.updatetime = 300					-- faster completion (4000ms default)
+vim.opt.expandtab = true					-- convert tabs (--->) to spaces (....)
+vim.opt.shiftwidth = 4						-- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 						-- insert 4 spaces (....) for a tab (--->)
+vim.opt.termguicolors = true                -- enable more colors inside the terminal (most terminals support this)
+
